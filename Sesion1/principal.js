2277695c8e6fa0824d220c8dd79f3c6cd100c210
marmelendez <lizbethmelendez@live.com>
2022-10-18 20:11:40 -0500

    Create new module

---
+++ Sesion1/principal.js
@@ -0,0 +1,12 @@
+// Modulos que nosotros mismos creamos
+
+// Classic function
+function sumarFn(num1, num2) {
+    console.log(num1 + num2);
+}
+
+// Arrow function
+// const sumar = (num1, num2) => console.log(num1 + num2);
+
+// Ya se puede utilizar en otros archivos
+module.exports = sumarFn;

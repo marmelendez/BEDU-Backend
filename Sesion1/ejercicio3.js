1385930fb2234bd810bf5862ad10648ac98bca66
marmelendez <lizbethmelendez@live.com>
2022-10-18 21:28:36 -0500

    Solve server with variables challenge

--- Sesion1/ejercicio3.js
+++ Sesion1/ejercicio3.js
@@ -1,13 +1,29 @@
 const http = require('http'); // de sistema
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3003;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain') 
-    res.end('Hola mundo!')
+    res.statusCode = 418;
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({
+        success: true,
+        canIGetCoffee: false,
+        responseCode: 0123
+    }))
 })
 
 server.listen(port, hostname, () => {
     console.log('Servidor corriendo')
 })
+
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain') 
+//     res.end('Hola mundo!')
+// })
+
+// server.listen(port, hostname, () => {
+//     console.log('Servidor corriendo')
+// })

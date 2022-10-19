b9c5a11b4c594936c412d940140855fdcb9d1eda
marmelendez <lizbethmelendez@live.com>
2022-10-18 20:12:04 -0500

    Add doc to ejercicio1.js

--- Sesion1/ejercicio1.js
+++ Sesion1/ejercicio1.js
@@ -1,3 +1,9 @@
+// Modulos integrados
 let os = require('os');
 let misCpu = os.cpus();
 console.log(misCpu);
+
+// Modulos de terceros
+let moment = require('moment');
+let now = moment();
+console.log(now);

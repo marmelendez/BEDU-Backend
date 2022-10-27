32c7133572077e0e6a212eb8ac14524e1131641c
marmelendez <lizbethmelendez@live.com>
2022-10-26 19:36:45 -0500

    Check setTimeout function

--- Sesion3/server.js
+++ Sesion3/server.js
@@ -1,3 +1,4 @@
+/*
 let funcion = (num1, num2, callback) => {
     callback("holaa")
     let res = num1 + num2
@@ -5,3 +6,17 @@ let funcion = (num1, num2, callback) => {
 }
 
 funcion(1,2, (message) => console.log(message))
+*/
+
+let fm1 = () => {
+    setTimeout(() => console.log(1), 2000)
+}
+
+let fm2 = () => console.log(2)
+let fm3 = () => console.log(3)
+
+fm1()
+fm2()
+fm3()
+
+

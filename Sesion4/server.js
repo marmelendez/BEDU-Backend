6998f14fe945f26ca7f40216eca96ba7b1c31e52
marmelendez <lizbethmelendez@live.com>
2022-10-27 19:56:18 -0500

    Realizar reto 2 de sesion 4

--- Sesion4/server.js
+++ Sesion4/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const PORT = 8080
+
+app.get('/html', (req,res) => {
+    res.setHeader('Content-Type', 'text/plain')
+    const html = fs.readFileSync('index.html')
+    res.send(html);
+})
 
 app.get('/', (req, res) => {
     res.send('Hola mundo')

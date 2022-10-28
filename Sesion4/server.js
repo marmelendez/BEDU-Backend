6f1a2e9d6bf7ab8888bb9ccedd70913acdb5e337
marmelendez <lizbethmelendez@live.com>
2022-10-27 21:11:28 -0500

    Regresar un JSON

--- Sesion4/server.js
+++ Sesion4/server.js
@@ -3,16 +3,26 @@ const fs = require('fs')
 const app = express()
 const PORT = 8080
 
+app.listen(PORT, () => {
+    console.log(`App corriendo en puerto ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('<h1>Mi API</h1>')
+})
+
 app.get('/html', (req,res) => {
     res.setHeader('Content-Type', 'text/plain')
     const html = fs.readFileSync('index.html')
     res.send(html);
 })
 
-app.get('/', (req, res) => {
-    res.send('Hola mundo')
-})
+const notes = [{
+    id: 1,
+    title: 'dummy',
+    content: 'nota dummy'
+}]
 
-app.listen(PORT, () => {
-    console.log(`App corriendo en puerto ${PORT}`)
+app.get('/notes', (req, res) => {
+    res.send(notes)
 })

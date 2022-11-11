2e6b4c3dda46447215f2f38e36c03b7eff7dee7f
marmelendez <lizbethmelendez@live.com>
2022-11-10 19:15:37 -0600

    Initial files from Sesion8

--- Sesion5/routes/index.js
+++ Sesion5/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-router.user('/notes', require('./notes'))
+router.use('/notes', require('./notes'))
 
 module.exports = router
--- Sesion5/server.js
+++ Sesion5/server.js
@@ -3,18 +3,14 @@ const fs = require('fs')
 const app = express()
 const PORT = 8080
 
+app.use(express.json())
+
 app.listen(PORT, () => {
     console.log(`App corriendo en puerto ${PORT}`)
-})
+}) 
 
 app.get('/', (req, res) => {
     res.send('<h1>Mi API</h1>')
 })
 
-app.get('/html', (req,res) => {
-    res.setHeader('Content-Type', 'text/plain')
-    const html = fs.readFileSync('index.html')
-    res.send(html);
-})
-
-app.get('/api/notes', require('./routes/index.js'))
+app.get('/api/', require('./routes/index.js'))
